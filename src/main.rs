3f615ad2c5b2d45ca39e2e56484c31fc0119567b
chris <kakaxi77749@gmail.com>
2022-06-17 13:19:14 +0800

    stable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use lucian;
-pub mod gate;
+use lucian::app::App;
+use lucian::hub::line::LineType;
+
+const CALLER_NUM: u8 = 30;
+const LISTEN_ADDR:&str = "127.0.0.1:1080";
+const PROXY_SERVER:&str = "8.218.15.102:3389";
 
 fn main() {
-    
+    let mut app = App::new(LISTEN_ADDR,LineType::Fox);
+    app.init(CALLER_NUM,PROXY_SERVER);
+    app.start();
 }
