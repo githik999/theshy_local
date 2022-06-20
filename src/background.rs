214c72ad938ce580be2f16f014073ca8508b273b
chris <kakaxi77749@gmail.com>
2022-06-21 02:47:16 +0800

    log

--- src/background.rs
+++ src/background.rs
@@ -1,5 +1,4 @@
 use lucian::gate::hub::line_header::LineType;
-use lucian::log::Log;
 use lucian::server::Server;
 
 const CALLER_NUM: u8 = 16;
@@ -8,7 +7,6 @@ const LOG_ADDR:&str = "0.0.0.0:1081";
 const PROXY_SERVER:&str = "8.218.15.102:3389";
 
 pub fn start() {
-    Log::init();
     let mut app = Server::new(APP_ADDR,LineType::Fox);
     app.init(CALLER_NUM,PROXY_SERVER);
     let mut http = Server::new(LOG_ADDR,LineType::Http);
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use lucian::log::Log;
+
 mod ui;
 mod background;
 
 fn main() {
+    Log::init();
 
     std::thread::spawn(||{
         background::start();
